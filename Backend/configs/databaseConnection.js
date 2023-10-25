be4bd30a9094817b7ecdeb183fced356035e5d63
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-10-25 13:51:38 +0600

    docker compose

--- Backend/configs/databaseConnection.js
+++ Backend/configs/databaseConnection.js
@@ -1,20 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async (callback) => {
-    try {
-        const dbConnectionString = process.env.DB_CONNECTION_STRING;
-        if (dbConnectionString) {
-            const client = mongoose.connect(dbConnectionString);
-            if (client) {
-                console.log('Database connected successfully');
-                callback();
-            } else {
-                console.log('Database url is not provided');
-            }
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const dbConnectionString =
+      "mongodb://mongo-container-compose:27017/mid-term-project";
+    console.log("connection string", dbConnectionString);
+    if (dbConnectionString) {
+      const client = await mongoose.connect(dbConnectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, // Add this line
+      });
+      if (client) {
+        console.log("Database connected successfully.By docker");
+        callback();
+      } else {
+        console.log("Database url is not provided");
+      }
     }
+  } catch (error) {
+    console.log("sssss error", error);
+  }
 };
 
 module.exports = connectDB;
